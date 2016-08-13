c4047d9f396792928d5626e7f52849d3b7ce6beb
ODevin <543237101@qq.com>
2016-08-14 05:21:13 +0800

    testgit

--- twisted/t1.py
+++ twisted/t1.py
@@ -1,16 +1,6 @@
 from twisted.internet import reactor,endpoints
 from twisted.internet.protocol import Factory,Protocol
 from twisted.protocols.basic import LineReceiver
-
-class rece(LineReceiver):
-    def lineReceived(self, line, data):
-        recedata=line.decode()
-        if recedata in data:
-            print("yes,in this")
-        else:
-            self.sendLine(b"i do not what you mean")
-
-
 class Echo(Protocol):
     dis={"a":"you choice a","b":"you choice b"}
 
@@ -19,17 +9,12 @@ class Echo(Protocol):
 
     def dataReceived(self, data):
         res=data.decode().strip()
-        # res=data.decode().replace("\r\n","").replace("")
-        # print(data.decode())
         print(res)
-        # print(self.dis.keys())
-        print(res == "a")
-        # print("a" in self.dis.keys())
         if res in self.dis:
             print("yes,in this")
-            self.transport.write(b'enen'+self.dis[res].encode())
+            self.transport.write(b'enen'+self.dis[res].encode()+b'\r\n')
         else:
-            self.transport.write(b'i do not what you mean')
+            self.transport.write(b'i do not what you mean\r\n')
 factory=Factory()
 factory.protocol=Echo
 reactor.listenTCP(4444,factory)
